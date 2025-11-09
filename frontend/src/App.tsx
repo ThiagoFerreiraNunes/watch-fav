c7c3631e1bfaf5cf1b2e78308d698398151ee437
ThiagoFerreiraNunes <thiagonflive@gmail.com>
2025-11-08 21:27:16 -0300

    Criando estrutura de pastas com React

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+import "./App.css";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes></Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
