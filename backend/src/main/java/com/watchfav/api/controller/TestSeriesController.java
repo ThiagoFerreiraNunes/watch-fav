832c66b2085b4b782cf654800b60033998709195
ThiagoFerreiraNunes <thiagonflive@gmail.com>
2025-11-10 16:36:48 -0300

    Adicionando anotação de @CrossOrigin para http://localhost:5173

--- backend/src/main/java/com/watchfav/api/controller/TestSeriesController.java
+++ backend/src/main/java/com/watchfav/api/controller/TestSeriesController.java
@@ -3,10 +3,12 @@ package com.watchfav.api.controller;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/series")
 public class TestSeriesController {
