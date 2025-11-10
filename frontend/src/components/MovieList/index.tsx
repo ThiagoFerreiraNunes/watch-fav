8032c5616c08d7f1093c51a224dd5e2373a8702f
ThiagoFerreiraNunes <thiagonflive@gmail.com>
2025-11-10 16:38:26 -0300

    Modificando páginas para consumir requisições GET do teste da api

--- frontend/src/components/MovieList/index.tsx
+++ frontend/src/components/MovieList/index.tsx
@@ -1,18 +1,36 @@
 import * as S from "./styles";
-import moviesData from "../../json/fakeApiMovies.json";
-import type { Movie } from "../../interfaces/Movie/Movie";
 import { MovieItem } from "./MovieItem";
-
-interface MovieData {
-  movies: Movie[];
-}
-
-const typedMoviesData = moviesData as MovieData;
+import type { Movie } from "../../interfaces/Movie/Movie";
+import { useEffect, useState } from "react";
 
 export const MovieList = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/movies")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Erro ao buscar filmes");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setMovies(data.movies);
+      })
+      .catch((error) => {
+        console.error("Erro:", error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Carregando...</p>;
+
   return (
     <S.Container>
-      {typedMoviesData.movies.map((movie) => (
+      {movies.map((movie) => (
         <MovieItem key={movie.id} movie={movie} />
       ))}
     </S.Container>
--- frontend/src/components/SeriesList/index.tsx
+++ frontend/src/components/SeriesList/index.tsx
@@ -1,18 +1,36 @@
 import * as S from "./styles";
-import seriesData from "../../json/fakeApiSeries.json";
-import type { Series } from "../../interfaces/Series/Series";
 import { SeriesItem } from "./SeriesItem";
-
-interface SeriesData {
-  series: Series[];
-}
-
-const typedSeriesData = seriesData as SeriesData;
+import type { Series } from "../../interfaces/Series/Series";
+import { useEffect, useState } from "react";
 
 export const SeriesList = () => {
+  const [series, setSeries] = useState<Series[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/series")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Erro ao buscar series");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setSeries(data.series);
+      })
+      .catch((error) => {
+        console.error("Erro:", error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Carregando...</p>;
+
   return (
     <S.Container>
-      {typedSeriesData.series.map((series) => (
+      {series.map((series) => (
         <SeriesItem key={series.id} series={series} />
       ))}
     </S.Container>
