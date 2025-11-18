7e6e101097d36d94a92be4ef9ca424955628da71
ThiagoFerreiraNunes <thiagonflive@gmail.com>
2025-11-18 11:05:29 -0300

    Modificando funcionalidades de países para se adequar a JPA

--- backend/src/main/java/com/watchfav/api/model/Country.java
+++ backend/src/main/java/com/watchfav/api/model/Country.java
@@ -3,11 +3,30 @@ package com.watchfav.api.model;
 import com.watchfav.api.dto.country.PostCountryDTO;
 import com.watchfav.api.dto.country.PutCountryDTO;
 import com.watchfav.api.model.common.HasAvailability;
+import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Table(name = "tb_countries")
+@Entity(name = "Country")
 public class Country implements HasAvailability {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "country_id")
     private Long id;
+
+    @Column(name = "country_name")
     private String name;
+
+    @Column(name = "is_available")
     private Boolean isAvailable;
+
+    @OneToMany(mappedBy = "country")
+    private List<Movie> movies = new ArrayList<>();
+
+    @OneToMany(mappedBy = "country")
+    private List<Actor> actors = new ArrayList<>();
 
     public Country(){}
 
@@ -16,26 +35,12 @@ public class Country implements HasAvailability {
         this.isAvailable = true;
     }
 
-    public Country(Long id, String name, Boolean isAvailable) {
-        this.id = id;
-        this.name = name;
-        this.isAvailable = isAvailable;
-    }
-
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     @Override
@@ -43,10 +48,13 @@ public class Country implements HasAvailability {
         return isAvailable;
     }
 
-    public void setIsAvailable(Boolean isAvailable) {
-        this.isAvailable = isAvailable;
+    public List<Movie> getMovies() {
+        return movies;
     }
 
+    public List<Actor> getActors() {
+        return actors;
+    }
 
     public void updateData(PutCountryDTO data){
         if(data.name() != null) name = data.name();
--- backend/src/main/java/com/watchfav/api/service/country/CountryService.java
+++ backend/src/main/java/com/watchfav/api/service/country/CountryService.java
@@ -1,15 +1,15 @@
 package com.watchfav.api.service.country;
 
 import com.watchfav.api.dto.country.PutCountryDTO;
-import com.watchfav.api.exception.ResourceNotFoundException;
 import com.watchfav.api.dto.country.GetCountryDTO;
 import com.watchfav.api.dto.country.PostCountryDTO;
 import com.watchfav.api.model.Country;
 import com.watchfav.api.exception.BusinessRuleException;
-import com.watchfav.api.repository.CountryDAO;
+import com.watchfav.api.repository.CountryRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,22 +17,23 @@ import java.util.List;
 public class CountryService {
 
     @Autowired
-    private CountryDAO countryDAO;
+    private CountryRepository countryRepository;
 
     @Transactional
     public GetCountryDTO postACountry(PostCountryDTO data){
         Country country = new Country(data);
-        country = countryDAO.save(country);
+        countryRepository.save(country);
+
         return new GetCountryDTO(country);
     }
 
     public List<GetCountryDTO> getAllCountries(){
-        return countryDAO.findAllAvailableAndSort().stream().map(GetCountryDTO::new).toList();
+        return countryRepository.findAllByAvailableAndSort().stream().map(GetCountryDTO::new).toList();
     }
 
     public GetCountryDTO getACountry(Long id){
-        Country country = countryDAO.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Country not found."));
+        Country country = countryRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Country not found."));
 
         if(Boolean.FALSE.equals(country.getIsAvailable())){
             throw new BusinessRuleException("Country is deleted.");
@@ -41,41 +42,41 @@ public class CountryService {
         return new GetCountryDTO(country);
     }
 
-    @Transactional
+    @jakarta.transaction.Transactional
     public GetCountryDTO putACountry(Long id, PutCountryDTO data){
-        Country country = countryDAO.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Country not found."));
+        Country country = countryRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Country not found."));
 
         if(Boolean.FALSE.equals(country.getIsAvailable())){
             throw new BusinessRuleException("Country is deleted.");
         }
+
         country.updateData(data);
-        countryDAO.save(country);
         return new GetCountryDTO(country);
     }
 
-    @Transactional
+    @jakarta.transaction.Transactional
     public void deleteACountry(Long id){
-        Country country = countryDAO.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Country not found."));
+        Country country = countryRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Country not found."));
 
         if(Boolean.FALSE.equals(country.getIsAvailable())){
             throw new BusinessRuleException("Country is already deleted.");
         }
+
         country.delete();
-        countryDAO.updateIsAvailable(id, country.getIsAvailable());
     }
 
     @Transactional
     public GetCountryDTO reactivateACountry(Long id){
-        Country country = countryDAO.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Country not found."));
+        Country country = countryRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Country not found."));
 
         if(Boolean.TRUE.equals(country.getIsAvailable())){
             throw new BusinessRuleException("Country is already active.");
         }
+
         country.reactivate();
-        countryDAO.updateIsAvailable(id, country.getIsAvailable());
         return new GetCountryDTO(country);
     }
 }
